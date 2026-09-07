104b1e8dd0a15344a3032ecedfb91860580af623
manishmamidipally <mamidipallymanish6@gmail.com>
2026-09-07 14:30:05 +0530

    Searching

---
+++ DSA_Problems/Searching/FindTheElement.java
@@ -0,0 +1,21 @@
+package DSA_Problems.Searching;
+
+public class FindTheElement {
+
+    public static void main(String args[]){
+        int arr[] = {23,89,18,37,49};
+
+        int target = 18;
+        int index = -1;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == target){
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println(index!=-1?"The Element is Found : "+index:"not Found");
+    }
+    
+}
