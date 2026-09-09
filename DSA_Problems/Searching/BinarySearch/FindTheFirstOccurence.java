6d6a6aba48dc0494261a5ef582354dc361d2927b
manishmamidipally <mamidipallymanish6@gmail.com>
2026-09-09 14:41:14 +0530

    Added The FindTheFirstOccurence

---
+++ DSA_Problems/Searching/BinarySearch/FindTheFirstOccurence.java
@@ -0,0 +1,33 @@
+package DSA_Problems.Searching.BinarySearch;
+
+public class FindTheFirstOccurence {
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,2,2,2,2,2,2,2,3,4};
+
+        int index = -1;
+        int low =0;
+        int target = 2;
+        int high = arr.length-1;
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+
+            if(arr[mid]==target){
+                index = low;
+                high=mid-1;
+            }
+            else if(arr[mid]>target){
+                high = mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+
+        System.out.println(index);
+
+
+    }
+    
+}
