25abf5a8fcf4de4efb3e106b251dfd8473344a5f
manishmamidipally <mamidipallymanish6@gmail.com>
2026-09-09 14:52:50 +0530

    LastOccurence

---
+++ DSA_Problems/Searching/BinarySearch/FindTheLastOcurence.java
@@ -0,0 +1,31 @@
+package DSA_Problems.Searching.BinarySearch;
+
+public class FindTheLastOcurence {
+    public static void main(String[] args) {
+        
+        int arr[]={1,2,2,2,2,2,2,4};
+
+        int target = 2;
+        int low=0;
+        int high = arr.length-1;
+        int index = -1;
+
+        while(low<=high){
+
+            int mid = low+(high-low)/2;
+            if(arr[mid]==target){
+                index=mid;
+                low=mid+1;
+            }
+            else if(arr[mid]>target){
+                low=mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+
+        }
+        System.out.println(index);
+    }
+    
+}
