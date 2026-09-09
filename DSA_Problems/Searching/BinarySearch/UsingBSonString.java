1dc38dc0304807a84ebf1ac92f53f68604dd2171
manishmamidipally <mamidipallymanish6@gmail.com>
2026-09-09 14:21:45 +0530

    Added The BinarySearch Uisng Strings

---
+++ DSA_Problems/Searching/BinarySearch/UsingBSonString.java
@@ -0,0 +1,37 @@
+package DSA_Problems.Searching.BinarySearch;
+
+public class UsingBSonString {
+    public static void main(String[] args) {
+        
+        String names[] = {"apple","banana","cat","dog","eagel"};
+        String target ="cat";
+
+        int low =0;
+        int high = names.length-1;
+     
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+
+            int result = names[mid].compareTo(target);
+
+            if(result == 0){
+                System.out.println("Target value id found "+mid);
+                return;
+            }
+            else if(result < 0){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+
+
+        }
+
+        System.out.println("No element is found ");
+        
+
+
+    }
+}
